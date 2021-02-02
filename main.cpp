c725c36d1ac08bcf44985b8b4f0b8d1365f582c2
Louis Christner <louis.christner@epitech.eu>
2021-02-02 16:48:15 +0100

    Add framerate limiter in main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-#include <chrono>
-
 #include "headers/SDL2Renderer.hpp"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+#define FPS 60
+
 int main(void)
 {
+    const int ticksPerFrame = 1000 / FPS;
     SDL2Renderer window({WINDOW_WIDTH, WINDOW_HEIGHT});
     std::list<const Entity *> entities;
 
@@ -20,12 +21,21 @@ int main(void)
 
     SDL_Event event;
 
+    Uint32 startTime = SDL_GetTicks();
+
     while (true) {
+        startTime = SDL_GetTicks();
+
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT)
             break;
 
         window.drawEntities(entities);
+
+        int frameTicks = SDL_GetTicks() - startTime;
+        if (frameTicks < ticksPerFrame) {
+            SDL_Delay(ticksPerFrame - frameTicks);
+        }
     }
 
     return 0;
