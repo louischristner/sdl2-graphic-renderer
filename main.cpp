61153cb16ce8503e53dbeb69acc82e87103118fd
Louis Christner <louis.christner@epitech.eu>
2021-02-03 18:20:14 +0100

    Update main exemple

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <ctime>
+#include <cstdlib>
+
 #include "headers/SDL2Renderer.hpp"
 
 #define WINDOW_WIDTH 800
@@ -12,16 +15,18 @@ int main(void)
     std::list<const Entity *> entities;
 
     Entity rect = window.createRectangle({100, 100});
+    rect.setColor({255, 255, 255});
     entities.push_back(&rect);
-
-    Entity sprite = window.createSprite("resources/img.png", {100, 100});
-    entities.push_back(&sprite);
 
     // game loop
 
-    SDL_Event event;
+    std::srand(std::time(nullptr));
 
-    Uint32 startTime = SDL_GetTicks();
+    Vect2<float> entitySpeed = {10, 10};
+
+    SDL_Event event;
+    Uint32 startTime =
+        SDL_GetTicks();
 
     while (true) {
         startTime = SDL_GetTicks();
@@ -36,6 +41,28 @@ int main(void)
         if (frameTicks < ticksPerFrame) {
             SDL_Delay(ticksPerFrame - frameTicks);
         }
+
+        Color color = {
+            (uint8_t) (std::rand() % 255),
+            (uint8_t) (std::rand() % 255),
+            (uint8_t) (std::rand() % 255)
+        };
+
+        if (rect.getPosition().x < 0) {
+            entitySpeed.x = -entitySpeed.x;
+            rect.setColor(color);
+        } else if (rect.getPosition().y < 0) {
+            entitySpeed.y = -entitySpeed.y;
+            rect.setColor(color);
+        } else if (rect.getPosition().x + rect.getSize().x > WINDOW_WIDTH) {
+            entitySpeed.x = -entitySpeed.x;
+            rect.setColor(color);
+        } else if (rect.getPosition().y + rect.getSize().y > WINDOW_HEIGHT) {
+            entitySpeed.y = -entitySpeed.y;
+            rect.setColor(color);
+        }
+
+        rect.translate(entitySpeed * ((float) ticksPerFrame / FPS));
     }
 
     return 0;
